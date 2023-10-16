0795cf51a2dc26a43712b433deffd23b37c382f6
SwayKh <khatrisumit712@gmail.com>
2023-10-16 22:15:56 +0530

    Done with the logic, need to display the output in HTML

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 const tableArea = document.querySelector('.table-area')
-const input = document.querySelector('.input')
+const textArea = document.querySelector('textarea')
 const start = document.querySelector('.start')
 const topicBtn = document.querySelector('.copy-topic-btn')
 const numBtn = document.querySelector('copy-num-btn')
 
 
 // # Create a dictionary to store the counts
-name_counts = {}
 
-// # Read names from the input file
-// with open('TopicsEntry.txt', 'r') as file:
-// for line in file:
-//     name = line.strip()  # Assuming each name is on a new line
-// if name in name_counts:
-//     name_counts[name] += 1
-// else:
-// name_counts[name] = 1
+let name_counts = {}
 
-// # Create a file to store unique names and another file to store the counts
-// with open('unique_names.txt', 'w') as unique_file, open('name_counts.txt', 'w') as counts_file:
-// for name, count in name_counts.items():
-//     unique_file.write(f'{name}\n')
-// counts_file.write(f'{count}\n')
+start.addEventListener('click', () => {
+    let inputArray = textArea.value.split('\n')
+    inputArray.forEach(element => {
+        if (element in name_counts) {
+            name_counts[element] += 1
+        } else {
+            name_counts[element] = 1
+        }
+    }
+    );
+    console.table(name_counts)
+    // console.log(Object.keys(name_counts).length)
+    // tableArea.textContent = name_counts
+})
